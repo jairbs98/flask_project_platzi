d47caa869ee6fb5e436948aac826a09da67b0106
edgar.beltran <edgar.beltran@coppel.com>
2023-08-22 15:36:52 -0700

    Clase 14. Configurar páginas de error y 15. Flask Bootstrap

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 
 todos = ['Carlon joto', 'Roberto gay', 'Angel chivo', 'Chap joto']
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html', error=error)
 
 
 @app.route('/')
