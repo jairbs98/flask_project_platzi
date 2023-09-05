0007b87e9b339a8d53e78d96d81e36e745b27069
edgar.beltran <edgar.beltran@coppel.com>
2023-09-04 17:36:41 -0700

    Clase 21, 22, 23 y 24

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
+from app import create_app
+from app.forms import LoginForm
 
-app = Flask(__name__)
-app.config.update(
-    DEBUG=False,
-    ENV='development'
-)
+app = create_app()
 
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
-
-bootstrap = Bootstrap(app)
-
-todos = ['Carlon joto', 'Roberto gay', 'Angel chivo', 'Chap joto', 'Brandon pajero']
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
+todos = ['1', '2', '3', '4', '5']
 
 
 @app.cli.command()
